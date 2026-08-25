6c0b738d6a7e4e8e1e2f026ae8dc35e1f09cdaeb
Hardhik <dhangerhardhik25@gmail.com>
2026-08-26 00:45:00 +0530

    feat: nested loop patterns

---
+++ pattern/charpattern.java
@@ -0,0 +1,12 @@
+public class charpattern{
+    public static void main(String args []) {
+        int n = 4;
+        char ch = 'A';
+        for (int line = 1; line <= n; line++) {
+            for (int num = 1; num <= line ; num++) {     
+                System.out.print(ch);
+                ch++;
+            } System.out.println();
+        }
+    }
+}
